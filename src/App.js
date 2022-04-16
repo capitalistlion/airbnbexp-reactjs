4edd0f4f43cab8f6b835ee0ca21ad8489b55f94f
Panashe Madzudzo <admin@asantech.dev>
2022-04-16 11:54:00 +0200

    sold out badge

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ export default function App() {
     document.title = "AirBnB Experiences"
   }, [])
 
-  const trainers = data.map(element => <Trainer trainerImg={element.coverImg} title={element.title} rating={element.stats.rating} reviewCount={element.stats.reviewCount} price={element.price} location={element.location} />)
+  const trainers = data.map(element => <Trainer trainerImg={element.coverImg} title={element.title} rating={element.stats.rating} reviewCount={element.stats.reviewCount} price={element.price} location={element.location} openSpots={element.openSpots} />)
 
   return (
       <div>
         <Navbar />
         <Hero />
         <section className="cards-list">
-        {trainers}
+          {trainers}
         </section>
       </div>
   )
--- src/components/Trainer.js
+++ src/components/Trainer.js
@@ -2,22 +2,21 @@ import React from "react"
 
 export default function Trainer(props) {
     return (
-        <section className="trainer-section">
-            <div className="card">
-                <img src={`../images/${props.trainerImg}`} className="card--image" alt="trainerImg" />
-                <div className="card--stats">
-                    <img src="../images/star.png" className="card--star" alt="starImg" />
-                    <span>{props.rating}</span>
-                    <span className="grey">{props.reviewCount} • </span>
-                    <span className="grey">{props.location}</span>
-                </div>
-                <p className="card--description">
-                    {props.title}
-                </p>
-                <p className="card--pricing">
-                    <strong>From ${props.price}</strong> / person
-                </p>
+        <div className="card">
+            {props.openSpots == 0 && <div className="card--badge">SOLD OUT</div>}
+            <img src={`../images/${props.trainerImg}`} className="card--image" alt="trainerImg" />
+            <div className="card--stats">
+                <img src="../images/star.png" className="card--star" alt="starImg" />
+                <span>{props.rating}</span>
+                <span className="grey">{props.reviewCount} • </span>
+                <span className="grey">{props.location}</span>
             </div>
-        </section>
+            <p className="card--description">
+                {props.title}
+            </p>
+            <p className="card--pricing">
+                <strong>From ${props.price}</strong> / person
+            </p>
+        </div>
     )
 }
